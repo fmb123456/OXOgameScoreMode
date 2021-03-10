840c0251c036e636e4572246f87c54e360220a8b
fmb123456 <79188860+fmb123456@users.noreply.github.com>
2021-03-10 11:15:01 +0800

    Update Game.js

--- Game.js
+++ Game.js
@@ -11,84 +11,74 @@ const lines = [
 class Table {
     constructor() {
         this.state = Array.from({ length: 9 }, () => 0);
-        this.winner = 0;
-        this.finish = false;
     }
-    update_winner_and_finish() {
-        if (this.winner)
-            return;
-        for (let line of lines) {
-            let tot = line.map(idx => this.state[idx]).reduce((s, i) => s + i);
-            if (tot === 3) this.winner = 1;
-            if (tot === -3) this.winner = -1;
-        }
-        if (this.winner || this.state.every(i => i !== 0))
-            this.finish = true;
+    score(p) {
+        var cnt = 0;
+        for (let line of lines)
+            if (line.every(i => this.state[i] === p))
+                ++cnt;
+        return cnt;
     }
     move(idx, p) {
         this.state[idx] = p;
-        this.update_winner_and_finish();
-        return this.winner !== 0;
+    }
+    get finish() {
+        return this.state.every(i => i !== 0);
     }
     delete(idx) {
-        var res = this.winner !== 0;
         this.state[idx] = 0;
-        this.winner = 0;
-        this.finish = false;
-        return res;
     }
 }
 
 class Game {
     constructor() {
         this.small = Array.from({ length: 9 }, () => new Table());
-        this.big = new Table();
         this.history = [];
     }
     valid(idx) {
-        if (this.winner || this.small[idx[0]].finish || this.small[idx[0]].state[idx[1]])
+        if (this.small[idx[0]].state[idx[1]] !== 0)
             return false;
-        return this.last_move === -1 || this.last_move === idx[0];
+        return this.lastMove === -1 || this.lastMove === idx[0];
     }
     move(idx) {
-        if (!idx) return false;
-        var res = this.small[idx[0]].move(idx[1], this.cur_player);
-        if (res)
-            this.big.move(idx[0], this.cur_player);
+        this.small[idx[0]].move(idx[1], this.curPlayer);
         this.history.push(idx);
-        return res;
     }
     undo() {
         var idx = this.history.pop();
-        if (this.small[idx[0]].delete(idx[1]))
-            return this.big.delete(idx[0]), true;
-        else
-            return false;
+        this.small[idx[0]].delete(idx[1]);
     }
     reset() {
         while (this.history.length)
             this.undo();
     }
-    get last_move() {
+    get lastMove() {
         if (!this.history.length) return -1;
         let idx = this.history[this.history.length - 1];
         if (this.small[idx[1]].finish)
             return -1;
         return idx[1];
     }
-    get cur_player() {
+    get curPlayer() {
         return this.history.length & 1 ? -1 : 1;
     }
     get finish() {
-        return this.big.finish || this.small.every(i => i.finish);
+        return this.small.every(i => i.finish);
+    }
+    score(p) {
+        return this.small.map(i => i.score(p)).reduce((s, i) => s + i);
     }
     get winner() {
-        return this.big.winner;
+        var scoreO = this.score(1);
+        var scoreX = this.score(-1);
+        if (scoreO > scoreX) return 1;
+        if (scoreX > scoreO) return -1;
+        return 0;
     }
     state(idx) {
             return this.small[idx[0]].state[idx[1]];
         } *
-        valid_moves() {
+        validMoves() {
             for (let i = 0; i < 9; ++i)
                 for (let j = 0; j < 9; ++j)
                     if (this.valid([i, j]))
@@ -98,116 +88,46 @@ class Game {
 class Player {
     constructor(params) {
         if (params === "random") {
-            let a = Array.from({ length: 4 }, () => Math.random()).sort((i, j) => i - j);
-            this.line_cnt = [a[0], a[1]];
-            this.ready_rate = [0, a[2], a[3]];
-            this.occupy = Math.random + 1;
-            this.s_op_rate = [Math.random];
-            this.s_op_rate.push(1 - this.s_op_rate[0]);
-            this.next_now_rate = [Math.random / 2];
-            this.next_now_rate.push(1 - this.next_now_rate[0]);
-            this.omega = [1, Math.random];
-            for (let i = 1; i < 7; ++i) this.omega.push(this.omega[i] * this.omega[1]);
-            this.alpha = Math.random + 1;
+            this.lineCnt = [Math.random, Math.random, Math.random];
+            this.lineCnt.sort((i, j) => i - j);
+            this.sOpRate = [Math.random];
+            this.sOpRate.push(1 - this.sOpRate[0]);
+            this.nextNowRate = [Math.random / 2];
+            this.nextNowRate.push(1 - this.nextNowRate[0]);
         } else {
             params = params.split(' ').map(i => parseFloat(i));
-            this.line_cnt = [params[0], params[1]];
-            this.ready_rate = [0, params[2], params[3]];
-            this.occupy = params[4];
-            this.s_op_rate = [params[5], 1 - params[5]];
-            this.next_now_rate = [params[6], 1 - params[6]];
-            this.omega = [1, params[7]]
-            this.alpha = params[8];
-            for (let i = 1; i < 7; ++i) this.omega.push(this.omega[i] * this.omega[1]);
+            this.lineCnt = [params[0], params[1], params[2]];
+            this.sOpRate = [params[3], 1 - params[3]];
+            this.nextNowRate = [params[4], 1 - params[4]];
         }
     }
-    best_move(game, dep) {
+    bestMove(game, dep) {
         var best;
-        var best_score = Number.NEGATIVE_INFINITY;
-        for (let i of game.valid_moves()) {
-            let score = this.get_rating(game, i, dep);
-            if (!best || score - best_score > Number.EPSILON)
-                best_score = score, best = i;
+        var bestScore = Number.NEGATIVE_INFINITY;
+        for (let i of game.validMoves()) {
+            let score = this.getRating(game, i, dep);
+            if (!best || score - bestScore > Number.EPSILON)
+                bestScore = score, best = i;
         }
         return best;
     }
-    get_small_attack_rating(game, idx, p) {
-        if (game.small[idx].finish)
-            return game.small[idx].winner === p ? this.occupy : 0;
-        var ready = Array.from({ length: 9 }, () => false);
+    getSmallAttackRating(game, idx, p) {
+        var tot = 0;
         for (let line of lines) {
-            line = line.filter(i => game.state([idx, i]) !== p);
-            if (line.length === 1 && game.state([idx, line[0]]) === 0)
-                ready[line[0]] = true;
-        }
-        var ready_score = this.ready_rate[Math.min(ready.filter(i => i === true).length, 2)];
-        var line_score = 0;
-        var all_line = [];
-        for (let line of lines) {
-            if (line.some(i => ready[i]))
-                continue;
-            if (line.some(i => game.state([idx, i]) === -p))
-                all_line.push(0);
-            else
-                all_line.push(this.line_cnt[line.filter(i => game.state([idx, i]) === p).length]);
-        }
-        if (all_line.length) {
-            all_line.sort((i, j) => j - i);
-            let base = 0;
-            for (let [k, i] of all_line.entries()) {
-                line_score += i * this.omega[k];
-                base += this.omega[k];
-            }
-            line_score /= base;
-        }
-        return (ready_score + line_score) / 2;
-    }
-    get_small_rating(game, idx, p) {
-        return this.get_small_attack_rating(game, idx, p) * this.s_op_rate[0] - this.get_small_attack_rating(game, idx, -p) * this.s_op_rate[1];
-    }
-    get_big_rating(game, p) {
-        if (game.finish) {
-            if (game.winner === p)
-                return Number.POSITIVE_INFINITY;
-            else if (game.winner === -p)
-                return Number.NEGATIVE_INFINITY;
-            else
-                return 0;
-        }
-        var small_rating = Array.from({ length: 9 }, (i, k) => this.get_small_rating(game, k, p));
-        var ready_score = 0;
-        var line_score = 0;
-        var all_line = [];
-        var ready = Array.from({ length: 9 }, () => false);
-        for (let line of lines) {
-            line = line.filter(i => game.small[i].winner !== p);
-            if (line.length === 1 && !game.small[line[0]].finish && !ready[line[0]]) {
-                ready[line[0]] = true;
-                ready_score += this.alpha ** small_rating[line[0]];
+            if (line.every(i => game.state([idx, i]) !== -p)) {
+                let cnt = line.filter(i => game.state([idx, i]) === p).length;
+                tot += cnt === 3 ? 1 : this.lineCnt[cnt];
             }
         }
-        for (let line of lines) {
-            if (line.some(i => ready[i]))
-                continue;
-            if (line.some(i => game.small[i].winner !== p && game.small[i].finish))
-                all_line.push(0);
-            else all_line.push(this.alpha ** (line.map(i => small_rating[i]).reduce((s, i) => s + i) / 3));
-        }
-        if (all_line.length) {
-            all_line.sort((i, j) => j - i);
-            let base = 0;
-            for (let [k, i] of all_line.entries()) {
-                line_score += i * this.omega[k];
-                base += this.omega[k];
-            }
-            line_score /= base;
-        }
-        return (ready_score + line_score) / 2;
+        return tot;
     }
-    get_rating(game, idx, dep) {
+    getSmallRating(game, idx, p) {
+        return this.getSmallAttackRating(game, idx, p) * this.sOpRate[0] - this.getSmallAttackRating(game, idx, -p) * this.sOpRate[1];
+    }
+    getRating(game, idx, dep) {
         if (dep === 0) return 0;
-        var p = game.cur_player;
-        var rat_s = -this.get_big_rating(game, p);
+        var p = game.curPlayer;
+        var ratS = -this.getSmallRating(game, idx[0], p);
         game.move(idx);
         if (game.finish) {
             let res;
@@ -218,10 +138,10 @@ class Player {
             game.undo();
             return res;
         }
-        rat_s += this.get_big_rating(game, p);
-        var op_best = this.best_move(game, dep - 1);
-        var rat_op = this.get_rating(game, op_best, dep - 1);
+        ratS += this.getSmallRating(game, idx[0], p);
+        var opBest = this.bestMove(game, dep - 1);
+        var ratOp = this.getRating(game, opBest, dep - 1);
         game.undo();
-        return rat_s * this.next_now_rate[1] - rat_op * this.next_now_rate[0];
+        return ratS * this.nextNowRate[1] - ratOp * this.nextNowRate[0];
     }
 }
